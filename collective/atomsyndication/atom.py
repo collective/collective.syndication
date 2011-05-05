b66457e1ef50a8c3df74ef9b27de4c5e171ceec4
Joaquín Rosales <globojorro@gmail.com>
2011-05-04 20:14:31 -0500

    Atom link appears only for syndicatable content and the site-root

--- collective/atomsyndication/atom.py
+++ collective/atomsyndication/atom.py
@@ -1,23 +1,23 @@
+import re
 from five import grok
+
 from zope.interface import Interface
-from zope.component import getMultiAdapter
+from zope.component import getMultiAdapter, getUtility
 from Products.CMFCore.utils import getToolByName
+
 from zope.publisher.interfaces.browser import IDefaultBrowserLayer
 from plone.app.layout.viewlets.interfaces import IHtmlHeadLinks
+
 from Products.ATContentTypes.interfaces.interfaces import IATContentType
+from Products.CMFPlone.interfaces import IPloneSiteRoot
+
+from plone.registry.interfaces import IRegistry
+from collective.atomsyndication.controlpanel import IAtomSettings
+
 grok.templatedir('templates')
 
 class IAtomSyndicationLayer(IDefaultBrowserLayer):
     """ Default browser layer for the package. """
-
-class AtomFeedView(grok.View):
-    grok.context(Interface)
-    grok.name('atom.xml')
-    grok.template('atom')
-    grok.layer(IAtomSyndicationLayer)
-
-    def update(self):
-        self.request.RESPONSE.setHeader('Content-Type', 'application/atom+xml;;charset=utf-8')
 
 class AtomLinkViewlet(grok.Viewlet):
     grok.context(Interface)
@@ -28,10 +28,55 @@ class AtomLinkViewlet(grok.Viewlet):
 
     def update(self):
         syntool = getToolByName(self.context, 'portal_syndication')
-        if syntool.isSyndicationAllowed(self.context):
-            self.allowed = True
-            context_state = getMultiAdapter((self.context, self.request),
-            name=u'plone_context_state')
-            self.url = '%s/atom.xml' % context_state.object_url()
+        registry = getUtility(IRegistry)
+        self.atom_settings = registry.forInterface(IAtomSettings)
+        self.allowed = False
+        if self.atom_settings.atom_enabled:
+            context_state = getMultiAdapter((self.context, self.request), name=u'plone_context_state')
+            if syntool.isSyndicationAllowed(self.context):
+                self.allowed = True
+                self.url = '%s/atom.xml' % context_state.object_url()
+            if context_state.is_portal_root():
+                self.allowed = True
+                self.url = '%s/atom.xml' % context_state.canonical_object_url()
+
+class AtomFeedView(grok.View):
+    grok.context(Interface)
+    grok.name('atom.xml')
+    grok.template('atom')
+    grok.layer(IAtomSyndicationLayer)
+
+    def update(self):
+        self.request.RESPONSE.setHeader('Content-Type', 'application/atom+xml;;charset=utf-8')
+        registry = getUtility(IRegistry)
+        self.atom_settings = registry.forInterface(IAtomSettings)
+        if IPloneSiteRoot.providedBy(self.context):
+            self.results = self.query_catalog({'review_state': 'published', 'portal_type': ('Collection', 'Folder')})
         else:
-            self.allowed = False
+            syn_tool = getToolByName(self.context, 'portal_syndication')
+            self.results = syn_tool.getSyndicatableContent(self.context)
+            #            self.results = self.query_catalog({'path': {'query': '/'.join(this_path), 'depth': 1},})
+
+    def query_catalog(self, query):
+        catalog = getToolByName(self.context, 'portal_catalog')
+        return catalog(query)
+
+    def atom_id_tag(self, context):
+        url_tool = getToolByName(self.context, 'portal_url')
+        portal = url_tool.getPortalObject()
+        root_url = portal.absolute_url()
+        cre_date = context.CreationDate()
+        mod_date = context.ModificationDate()
+        tag = u"tag:%s," % self.remove_proto(root_url)
+        return tag
+
+    def remove_proto(self, data):
+        p = re.compile(r'https?://')
+        p1 = p.sub('', data)
+        p2 = p1.split('/')
+
+    def getNavTree(self, _marker=[]):
+        context = aq_inner(self.context)
+        queryBuilder = getMultiAdapter((context, self.data), INavigationQueryBuilder)
+        strategy = getMultiAdapter((context, self.data), INavtreeStrategy)
+        return buildFolderTree(context, obj=context, query=queryBuilder(), strategy=strategy)
