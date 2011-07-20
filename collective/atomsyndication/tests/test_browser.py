ac4dc550ac169f78815c6ca4e82ce45dc7250927
Joaquín Rosales <globojorro@gmail.com>
2011-07-20 00:00:56 -0500

    Fix all feeds

--- collective/atomsyndication/tests/test_browser.py
+++ collective/atomsyndication/tests/test_browser.py
@@ -1,52 +1,18 @@
-import unittest
+import unittest2 as unittest
 import doctest
 
-from zope.testing import doctestunit
-from Testing import ZopeTestCase as ztc
+from plone.testing import layered
 
-from collective.atomsyndication.tests import base
+from collective.atomsyndication.testing import FUNCTIONAL_TESTING
 
-
-class FunctionalTestCase(base.FunctionalTestCase):
-
-    def loginAsAdmin(self):
-        from Products.PloneTestCase.setup import portal_owner, default_password
-
-        browser = self.browser
-        browser.open(self.portal.absolute_url() + "/login_form")
-        browser.getControl(name='__ac_name').value = portal_owner
-        browser.getControl(name='__ac_password').value = default_password
-        browser.getControl(name='submit').click()
-
-    def installPackage(self):
-        from Products.CMFCore.utils import getToolByName
-        qi = getToolByName(self.portal, 'portal_quickinstaller')
-        qi.installProduct('collective.atomsyndication')
-        return qi.isProductInstalled('collective.atomsyndication')
 
 def test_suite():
-    return unittest.TestSuite([
-
-        # Unit tests
-        #doctestunit.DocFileSuite(
-        #    'README.txt', package='collective.atomsyndication',
-        #    setUp=testing.setUp, tearDown=testing.tearDown),
-
-        #doctestunit.DocTestSuite(
-        #    module='collective.atomsyndication.mymodule',
-        #    setUp=testing.setUp, tearDown=testing.tearDown),
-
-
-        # Integration tests that use PloneTestCase
-        #ztc.ZopeDocFileSuite(
-        #    'README.txt', package='collective.atomsyndication',
-        #    test_class=TestCase),
-
-        ztc.FunctionalDocFileSuite(
-            'README.txt', package='collective.atomsyndication',
-            test_class=FunctionalTestCase,
-            optionflags=doctest.NORMALIZE_WHITESPACE | doctest.ELLIPSIS),
-        ])
+    suite = unittest.TestSuite()
+    suite.addTests([
+        layered(doctest.DocFileSuite('README.txt'), layer=FUNCTIONAL_TESTING),
+    ])
+    return suite
+        
 
 if __name__ == '__main__':
     unittest.main(defaultTest='test_suite')
