e5750e793a38c5c708041a495cbf9766afbdfcd2
hvelarde <hector.velarde@gmail.com>
2012-07-11 20:10:50 -0300

    fixed package distribution and buildout development configurations

--- README.txt
+++ README.txt
@@ -1,6 +1,23 @@
-Introduction
-============
+**************************
+collective.atomsyndication
+**************************
 
+.. contents:: Table of Contents
 
+Life, the Universe, and Everything
+----------------------------------
 
-This product may contain traces of nuts.
+TBD.
+
+Don't Panic
+-----------
+
+TBD.
+
+Mostly Harmless
+---------------
+
+Have an idea? Found a bug? Let us know by `opening a support ticket`_.
+
+.. _`opening a support ticket`: https://github.com/collective/collective.atomsyndication/issues
+
--- setup.py
+++ setup.py
@@ -3,28 +3,37 @@
 from setuptools import setup, find_packages
 import os
 
-version = '0.1'
+version = '1.0a1'
+long_description = open("README.txt").read() + "\n" + \
+                   open(os.path.join("docs", "INSTALL.txt")).read() + "\n" + \
+                   open(os.path.join("docs", "CREDITS.txt")).read() + "\n" + \
+                   open(os.path.join("docs", "HISTORY.txt")).read()
 
 setup(name='collective.atomsyndication',
       version=version,
       description="",
-      long_description=open(os.path.join("README.txt")).read() + "\n" +
-                       open(os.path.join("docs", "HISTORY.txt")).read(),
-      # Get more strings from
-      # http://pypi.python.org/pypi?:action=list_classifiers
+      long_description=long_description,
       classifiers=[
-        "Development Status :: 4 - Beta",
+        "Development Status :: 3 - Alpha",
+        "Environment :: Web Environment",
+        "Framework :: Plone",
         "Framework :: Plone :: 4.1",
-        "License :: OSI Approved :: GNU General Public License (GPL)",
+        "Framework :: Plone :: 4.2",
+        "Intended Audience :: End Users/Desktop",
+        "Intended Audience :: System Administrators",
+        "License :: OSI Approved :: GNU General Public License v2 (GPLv2)",
         "Operating System :: OS Independent",
-        "Programming Language :: JavaScript",
         "Programming Language :: Python",
+        "Programming Language :: Python :: 2.6",
+        "Programming Language :: Python :: 2.7",
+        "Topic :: Office/Business :: News/Diary",
+        "Topic :: Software Development :: Libraries :: Python Modules",
         ],
       keywords='plone atom syndication',
       author='Joaquín Rosales',
       author_email='globojorro@gmail.com',
       url='http://github.com/collective/collective.atomsyndication',
-      license='GPL',
+      license='GPLv2',
       packages=find_packages('src'),
       package_dir={'': 'src'},
       namespace_packages=['collective'],
