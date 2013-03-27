d84200ff8b36391f829bf9398667bf4aad4e331b
hvelarde <hector.velarde@gmail.com>
2013-03-27 16:23:17 -0300

    Preparing release 1.0a4

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from setuptools import setup, find_packages
 import os
 
-version = '1.0a4.dev0'
+version = '1.0a4'
 description = "Improved syndication for Plone sites providing Atom, iTunes, \
 NewsML 1, RSS 1.0 and RSS 2.0 feeds."
 long_description = \
